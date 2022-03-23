e3eb77a17890a017fc5ede387966186317e8673a
Salah Berrhou <salah.webdev@gmail.com>
2022-03-23 14:57:32 +0100

    version before final

--- js/scripts.js
+++ js/scripts.js
@@ -109,6 +109,39 @@
         }
     });
 
+    var imageSliderTwo = new Swiper('.image-slider-3', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false
+		},
+        loop: true,
+        spaceBetween: 30,
+        slidesPerView: 3,
+		breakpoints: {
+            // when window is <= 580px
+            580: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window is <= 768px
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window is <= 992px
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            // when window is <= 1200px
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 20
+            },
+
+        }
+    });
+
 
     /* Text Slider - Swiper */
 	var textSlider = new Swiper('.text-slider', {
